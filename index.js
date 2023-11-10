cd5fc85fc236374b5b2c09afcb534c04614a58e4
Luke O'Regan <oregan.luke@gmail.com>
2023-11-10 17:23:00 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+//Display Dodgy whitespace from illustrator files
 document.body.innerText = document.body.innerText.replace(/\x03/g, function(c) { return '\\x' + ('00' + c.charCodeAt(0).toString(16)).slice(-2); });
+
+
+//Remove it
+document.body.innerText = document.body.innerText.replace(/\x03/g, '');
